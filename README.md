498d8b66c0741369e4cb7452ecd34431d2ddd8a0
Chekka Nishitha <nishithanishi263@gmail.com>
2025-09-09 16:26:08 +0530

    Hero Section + Navbar Assignment

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hero Navbar Portfolio
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold">🚀 Hero + Navbar Demo Running!</h1>
+    </div>
+  );
+}
+
+export default App;
